a2412a799a84709b5d3b763a354c1da07066de1b
Bryant Richards <dragonseyeimages@gmail.com>
2019-09-14 17:34:17 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -3,14 +3,14 @@
 
 def key_for_min_value(name_hash)
   lowest_key = nil
-  smallest_value = nil
+  lowest_value = nil
   name_hash.each do |key, value|
-    if smallest_value == nil || value < smallest_value
-      smallest_value = value
-      smallest_key = key
+    if lowest_value == nil || value < lowest_value
+      lowest_value = value
+      lowest_key = key
     end
   end
-  smallest_key
+  lowest_key
 end
 
 end
